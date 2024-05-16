6efd71adc3f3bed22a20d28d5a702e546af3169d
RcnonS <ratchanon.somprasong@gmail.com>
2024-05-16 13:45:59 +0700

    ex5

--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,7 @@
 let companyName = "TechUp";
 
 // Start coding here
+for (let letter in companyName) {
+  let number = Number(letter) + 1;
+  console.log(`Number ${number} character is ${companyName[letter]}`);
+}
