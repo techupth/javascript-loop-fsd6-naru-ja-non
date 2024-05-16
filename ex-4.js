80e595c91106687718ce181e0f3d26a07dab707e
Athikhom_ra <athikhom_ra@outlook.com>
2024-05-16 13:53:16 +0700

    Assinment#1-6 'loop' NARu

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,10 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for (let index in restaurants) {
+  // console.log(index);
+  newRestaurants[index] = "Restaurant Name: " + restaurants[index];
+  //   newRestaurants[index] = `Restaurant Name: ${restaurants[index]}`;
+}
 
 console.log(newRestaurants);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,15 @@
 let companyName = "TechUp";
 
 // Start coding here
+// console.log(companyName.length);
+// for (let i = 0; i < companyName.length; i++) {
+//   // console.log(companyName[i]);
+//   console.log(` Number ${i + 1} character is ${companyName[i]}`);
+// }
+for (let numbers in companyName) {
+  // console.log(numbers);
+  console.log(
+    ` Number ${Number(numbers) + 1} character is ${companyName[numbers]}`
+  );
+  //   console.log(` Number ${+numbers + 1} character is ${companyName[numbers]}`);
+}
